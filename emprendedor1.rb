76fc9e2e0ef731aeae8d99f0cba7bffc82115a1c
Pablo <pquezada2009@hotmail.es>
2019-05-08 23:26:01 -0400

    emprendedor1.rb_emprendedor2.rb

---
+++ emprendedor1.rb
@@ -0,0 +1,8 @@
+p = ARGV[0].to_f
+us= ARGV[1].to_f
+g= ARGV[2].to_f
+ut= p*us-g
+i=0.35*ut
+
+utf=ut-i
+puts utf
---
+++ emprendedor2.rb
@@ -0,0 +1,5 @@
+c=ARGV[0].to_f
+nus=ARGV[1].to_f
+nusp=ARGV[2].to_f
+g=ARGV[3].to_f
+puts ut=c*(nus+nusp)-g
